dcc2e31fbe7225770cd6b4832296c358e1941484
ssig33 <mail@ssig33.com>
2017-01-19 21:04:26 +0900

    create endpoint

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/labstack/echo"
+	"net/http"
 	"os"
 )
 
@@ -10,5 +11,12 @@ func main() {
 	fmt.Println("vim-go")
 
 	e := echo.New()
+
+	e.POST("/check", func(c echo.Context) error {
+		params, _ := c.FormParams()
+		fmt.Println(string(params.Get("payload")))
+		return c.String(http.StatusOK, "OK")
+	})
+
 	e.Logger.Debug(e.Start("0.0.0.0:" + os.Getenv("PORT")))
 }
